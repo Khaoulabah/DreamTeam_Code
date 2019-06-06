11aaa059c249db8b267ff4b7aa024b16457820c4
mlundvall <maria.v.lundvall@gmail.com>
2019-06-06 14:24:23 -0700

    Update User.py

--- DreamTeam_Code/User.py
+++ DreamTeam_Code/User.py
@@ -1,4 +1,5 @@
 import ImageList
+import ItemsLibrary
 
 """
     The purpose of this class is to provide images from user into image list for regional layer. 
@@ -7,6 +8,7 @@ import ImageList
 class User:
     userImage = ""
     list = ImageList()
+    itemsLibrary = ItemsLibrary()
 
     """
     Insert user provided image into the list.
